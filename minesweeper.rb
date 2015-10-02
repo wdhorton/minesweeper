cd04e6e6ba9a014e5ec93f5266247f4fba735197
William Horton <wdt.horton@gmail.com>
2015-10-02 11:55:34 -0400

    Add set_tile_values and add_bombs.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,8 +1,27 @@
 class Board
 
+
+  ADJACENTS = [
+    [-1, -1],
+    [0, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 0],
+    [-1, 1],
+    [1, -1]
+  ]
+
+  attr_reader :grid
   def initialize(size = 9)
     @grid = Array.new(size) { Array.new(size) { Tile.new } }
   end
+
+  def[](pos)
+    x,y = pos
+    grid[x][y]
+  end
+
 
   def add_bombs(number = 15)
     bombs = 0
@@ -12,6 +31,31 @@ class Board
       bombs += 1
     end
   end
+
+  def set_tile_values
+    grid.each.with_index do |row, idx|
+      row.each.with_index do |tile, idy|
+        tile.value = count_bombs([idx, idy])
+      end
+    end
+
+    nil
+  end
+
+  def count_bombs(pos)
+    adjacent_spaces(pos).count { |sp| self[sp].bomb }
+  end
+
+  def adjacent_spaces(position)
+     x,y = position
+     adjacent_spaces = ADJACENTS.map { |space| [x + space.first, y + space.last] }
+     adjacent_spaces.select { |space| on_board?(space) }
+  end
+
+  def on_board?(loc)
+    loc.all? { |n| n.between?(0, grid.length - 1) }
+  end
+
 
 end
 
